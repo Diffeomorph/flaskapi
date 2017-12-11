48af20477f908b7f9226b0e2930a69cb475a8955
gf1721 <gf1721@bristol.ac.uk>
2017-12-11 18:07:29 +0000

    finalwebapi

--- api.py
+++ api.py
@@ -24,6 +24,18 @@ def addOne():
     languages.append(language)
     return jsonify({'languages': languages})
     
+#updates entries
+@app.route('/lang/<string:name>',methods=['PUT'])
+def editOne(name):
+    langs = [language for language in languages if language['name']==name]
+    langs[0]['name'] = request.json['name']
+    return jsonify({'language':langs[0]})
+
+@app.route('/lang/<string:name>',methods=['DELETE'])    
+def removeOne(name):
+    lang = [language for language in languages if language['name']==name]
+    languages.remove(lang[0])
+    return jsonify({'languages': languages})
     
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
